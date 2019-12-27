7dde96a29d61c34cd90651983787bcc7d85b194a
Chase McDermott <chasemcd1745@tamu.edu>
2019-12-26 22:21:15 -0600

    Day 5 part 1..

---
+++ 5/day5-1.py
@@ -0,0 +1,5 @@
+from Intcode.Intcode import Intcode
+
+intcode = Intcode('5/input.txt', True)
+
+intcode.run()
--- Intcode/Intcode.py
+++ Intcode/Intcode.py
@@ -2,23 +2,89 @@ class Intcode:
     def __init__(self, filename, display=True):
         self.filename = filename
         self.display = display
+        self.opPtr = 0
+        self.opcodes = []
 
-    def opcodeFunc(self, op, vals):
+    def op(self):
+        return int(str(self.opcodes[self.opPtr]).rjust(5, '0')[-2:])
+
+    def saveNewVal(self, index, val):
+        print('saving new val')
+        print('index: {}'.format(index))
+        print('val: {}'.format(val))
+        self.opcodes[index] = val
+
+    def getVal(self, index, mode=0):
+        # print('mode: {}'.format(mode))
+        # print('index: {}'.format(index))
+        if mode == 0:
+            # print(self.opcodes[index])
+            # print('val: {}'.format(self.opcodes[self.opcodes[index]]))
+            return self.opcodes[self.opcodes[index]]
+        elif mode == 1:
+            return self.opcodes[index]
+        else:
+            raise ValueError("Invalid mode: {}".format(mode))
+
+    def getParamModes(self, instruction, length):
+        # print(str(instruction))
+        # print(str(instruction).rjust(length, '0'))
+        # print('modes: {}'.format(list(reversed(str(instruction).rjust(length, '0')[:-2]))))
+        return list(map(lambda x:int(x), reversed(str(instruction).rjust(length, '0')[:-2])))
+
+    def getParams(self, modes, op):
+        if op == 1 or op == 2:
+            return [
+                self.getVal(self.opPtr+1, modes[0]),
+                self.getVal(self.opPtr+2, modes[1]),
+                self.getVal(self.opPtr+3, 1)
+                ]
+        elif op == 3 or op == 4:
+            return [
+                self.getVal(self.opPtr+1, 1)
+            ]
+        else:
+            raise ValueError("Invalid op: {}".format(op))
+
+    def opcodeFunc(self, op):
+        paramModes = self.getParamModes(self.opcodes[self.opPtr], 5)
+        # print('opptr: {}'.format(self.opPtr))
+        print('op: {}'.format(op))
+        print('modes: {}'.format(paramModes))
+        params = self.getParams(paramModes, op)
+        print('params: {}'.format(params))
+
         if op == 1:
-            return self.opcodes[vals[0]] + self.opcodes[vals[1]]
+            newVal = params[0] + params[1]
+            self.saveNewVal(params[2], newVal)
+            return 4
         elif op == 2:
-            return self.opcodes[vals[0]] * self.opcodes[vals[1]]
+            newVal = params[0] * params[1]
+            self.saveNewVal(params[2], newVal)
+            return 4
+        elif op == 3:
+            inputVal = input("Input integer: ")
+            self.saveNewVal(params[0], int(inputVal))
+            return 2
+        elif op == 4:
+            print(self.opcodes[params[0]])
+            return 2
+        else:
+            raise ValueError("Invalid op: {}".format(op))
 
-    def run(self, resetVals):
+
+    def run(self, resetVals=None):
         with open(self.filename) as file:
-            opPtr = 0
             self.opcodes = list(map(lambda op:int(op), file.readline().split(',')))
-            self.opcodes[1], self.opcodes[2] = resetVals
-            op = self.opcodes[opPtr]
-            while op != 99:
-                self.opcodes[self.opcodes[opPtr + 3]] = self.opcodeFunc(op, self.opcodes[opPtr+1:opPtr+3])
-                opPtr += 4
-                op = self.opcodes[opPtr]
+            if resetVals is not None:
+                self.opcodes[1], self.opcodes[2] = resetVals
+
+            while self.op() != 99:
+                print(self.opcodes[self.opPtr:self.opPtr+10])
+                print(self.opPtr)
+                incNum = self.opcodeFunc(self.op())
+                self.opPtr += incNum
+
         if self.display:
             print("Final value at index 0: {}".format(self.opcodes[0]))
 
