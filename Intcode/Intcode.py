bb38d05d849cc5a3d69531c830a0a64bb3e3b2b9
Chase McDermott <chasemcd1745@tamu.edu>
2019-12-26 23:44:37 -0600

    Day 5 part 2 🤙

--- Intcode/Intcode.py
+++ Intcode/Intcode.py
@@ -1,25 +1,23 @@
 class Intcode:
-    def __init__(self, filename, display=True):
+    def __init__(self, filename, display=False, debug=False):
         self.filename = filename
         self.display = display
         self.opPtr = 0
         self.opcodes = []
+        self.debug = debug
 
     def op(self):
         return int(str(self.opcodes[self.opPtr]).rjust(5, '0')[-2:])
 
     def saveNewVal(self, index, val):
-        # print('saving new val')
-        # print('index: {}'.format(index))
-        # print('val: {}'.format(val))
+        if self.debug:
+            print('saving new val')
+            print('index: {}'.format(index))
+            print('val: {}'.format(val))
         self.opcodes[index] = val
 
     def getVal(self, index, mode=0):
-        # print('mode: {}'.format(mode))
-        # print('index: {}'.format(index))
         if mode == 0:
-            # print(self.opcodes[index])
-            # print('val: {}'.format(self.opcodes[self.opcodes[index]]))
             return self.opcodes[self.opcodes[index]]
         elif mode == 1:
             return self.opcodes[index]
@@ -27,48 +25,75 @@ class Intcode:
             raise ValueError("Invalid mode: {}".format(mode))
 
     def getParamModes(self, instruction, length):
-        # print(str(instruction))
-        # print(str(instruction).rjust(length, '0'))
-        # print('modes: {}'.format(list(reversed(str(instruction).rjust(length, '0')[:-2]))))
         return list(map(lambda x:int(x), reversed(str(instruction).rjust(length, '0')[:-2])))
 
     def getParams(self, modes, op):
-        if op == 1 or op == 2:
+        # +, *, <, ==
+        if op == 1 or op == 2 or op == 7 or op == 8:
             return [
                 self.getVal(self.opPtr+1, modes[0]),
                 self.getVal(self.opPtr+2, modes[1]),
                 self.getVal(self.opPtr+3, 1)
                 ]
+        # input, output
         elif op == 3 or op == 4:
             return [
                 self.getVal(self.opPtr+1, 1)
             ]
+        # true, false
+        elif op == 5 or op == 6:
+             return [
+                self.getVal(self.opPtr+1, modes[0]),
+                self.getVal(self.opPtr+2, modes[1])
+             ]
         else:
             raise ValueError("Invalid op: {}".format(op))
 
     def opcodeFunc(self, op):
         paramModes = self.getParamModes(self.opcodes[self.opPtr], 5)
-        # print('opptr: {}'.format(self.opPtr))
-        # print('op: {}'.format(op))
-        # print('modes: {}'.format(paramModes))
         params = self.getParams(paramModes, op)
-        # print('params: {}'.format(params))
+        if self.debug:
+            print('\n\nopcodes: {}'.format(self.opcodes))
+            print('opptr: {}'.format(self.opPtr))
+            print('op: {}'.format(op))
+            print('modes: {}'.format(paramModes))
+            print('params: {}'.format(params))
 
         if op == 1:
             newVal = params[0] + params[1]
             self.saveNewVal(params[2], newVal)
-            return 4
+            return self.opPtr + 4
         elif op == 2:
             newVal = params[0] * params[1]
             self.saveNewVal(params[2], newVal)
-            return 4
+            return self.opPtr + 4
         elif op == 3:
             inputVal = input("Input integer: ")
             self.saveNewVal(params[0], int(inputVal))
-            return 2
+            return self.opPtr + 2
         elif op == 4:
             print(self.opcodes[params[0]])
-            return 2
+            return self.opPtr + 2
+        elif op == 5:
+            if params[0]:
+                # Jump to params[1]
+                return params[1]
+            else:
+                return self.opPtr + 3
+        elif op == 6:
+            if not params[0]:
+                # jump to params[1]
+                return params[1]
+            else:
+                return self.opPtr + 3
+        elif op == 7:
+            comp = params[0] < params[1]
+            self.saveNewVal(params[2], int(comp))
+            return self.opPtr + 4
+        elif op == 8:
+            comp = params[0] == params[1]
+            self.saveNewVal(params[2], int(comp))
+            return self.opPtr + 4
         else:
             raise ValueError("Invalid op: {}".format(op))
 
@@ -80,10 +105,8 @@ class Intcode:
                 self.opcodes[1], self.opcodes[2] = resetVals
 
             while self.op() != 99:
-                # print(self.opcodes[self.opPtr:self.opPtr+10])
-                # print(self.opPtr)
                 incNum = self.opcodeFunc(self.op())
-                self.opPtr += incNum
+                self.opPtr = incNum
 
         if self.display:
             print("Final value at index 0: {}".format(self.opcodes[0]))
